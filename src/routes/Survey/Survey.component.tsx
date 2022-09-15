2a7f21b4562c686ebd66052b6a01ddfa360cea5a
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2022-09-15 19:27:40 +0530

    Added npm packages

--- src/routes/Survey/Survey.component.tsx
+++ src/routes/Survey/Survey.component.tsx
@@ -1,5 +1,16 @@
+import './Survey.styles.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export const SurveyPage = () => {
     return (
-        <div>Survey page</div>
+        <Container>
+            <Row>
+                <Col>
+                    <div className="survey-container">Survey page</div>
+                </Col>
+            </Row>           
+        </Container>
     )
 }
